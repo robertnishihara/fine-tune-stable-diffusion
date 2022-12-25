cb590bda856f907bf19a3e8b1a6c3804ed69657c
Richard Liaw <rliaw@berkeley.edu>
2022-12-25 15:10:34 -0800

    base-reqs
    Signed-off-by: Richard Liaw <rliaw@berkeley.edu>

--- src/server.py
+++ src/server.py
@@ -1,1 +1,22 @@
-server.py
+"""Simple stable diffusion app.
+
+How to run:
+    $ uvicorn server:app --reload
+
+How to test:
+"""
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
